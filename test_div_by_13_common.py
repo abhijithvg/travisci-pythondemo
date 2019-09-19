e93245b77d779fc7402d58d273d5279c14276c46
abhijithvg <abhijithvg@gmail.com>
2019-09-19 10:01:12 +0530

    Code without any errors

--- test_div_by_13_common.py
+++ test_div_by_13_common.py
@@ -1,5 +1,5 @@
 import pytest
 
-def test_divisible_by_13_common(updated_value):
+def test_divisible_by_13_common(commoninput_value):
    # assert commoninput_value % 13 == 0 #To fail assertion
    assert commoninput_value % 13 != 0 #To pass assertion
